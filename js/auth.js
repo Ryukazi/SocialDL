21dd7e3fa31500aa63b4db52381a129a6f502899
Kangxrelz <158026107+Ryukazi@users.noreply.github.com>
2026-08-16 01:56:49 +0545

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -8,60 +8,83 @@ import {
     getAuth,
     GoogleAuthProvider,
     signInWithPopup,
-    signOut,
-    onAuthStateChanged
+    onAuthStateChanged,
+    signOut
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 import {
     getFirestore,
     collection,
     addDoc,
-    serverTimestamp,
-    query,
-    orderBy,
-    getDocs,
-    deleteDoc,
-    doc
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+/* =========================================
+   FIREBASE CONFIG
+========================================= */
 
 const firebaseConfig = {
     apiKey: "AIzaSyAgNlfkMZ6dyeyUClSKYRVuvzD0zNAzVok",
-    authDomain: "studio-8804238675-8735f.firebaseapp.com",
-    projectId: "studio-8804238675-8735f",
-    storageBucket: "studio-8804238675-8735f.firebasestorage.app",
-    messagingSenderId: "472948311884",
-    appId: "1:472948311884:web:38e438e14d0706cb7231e9"
+
+    authDomain:
+        "studio-8804238675-8735f.firebaseapp.com",
+
+    projectId:
+        "studio-8804238675-8735f",
+
+    storageBucket:
+        "studio-8804238675-8735f.firebasestorage.app",
+
+    messagingSenderId:
+        "472948311884",
+
+    appId:
+        "1:472948311884:web:38e438e14d0706cb7231e9"
 };
 
+/* =========================================
+   INITIALIZE FIREBASE
+========================================= */
 
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(
+        firebaseConfig
+    );
 
-const auth = getAuth(app);
+/* =========================================
+   AUTH
+========================================= */
 
-const db = getFirestore(app);
+const auth =
+    getAuth(app);
 
-const googleProvider = new GoogleAuthProvider();
+const googleProvider =
+    new GoogleAuthProvider();
 
+googleProvider.setCustomParameters({
+    prompt: "select_account"
+});
+
+/* =========================================
+   FIRESTORE
+========================================= */
+
+const db =
+    getFirestore(app);
+
+/* =========================================
+   EXPORTS
+========================================= */
 
 export {
-    app,
     auth,
-    db,
     googleProvider,
-
-    getAuth,
-    GoogleAuthProvider,
     signInWithPopup,
-    signOut,
     onAuthStateChanged,
+    signOut,
 
+    db,
     collection,
     addDoc,
-    serverTimestamp,
-    query,
-    orderBy,
-    getDocs,
-    deleteDoc,
-    doc
+    serverTimestamp
 };
